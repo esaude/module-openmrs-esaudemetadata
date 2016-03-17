a256a60a954622c85414b4777c5d5f7fcaf9496c
ningosi <nicotwendelee@gmail.com>
2016-03-17 11:09:24 +0300

    Added metadata to test the module

--- api/src/main/java/org/openmrs/module/esaudemetadata/ESaudeMetadataActivator.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/ESaudeMetadataActivator.java
@@ -11,12 +11,23 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.eSaudeMetadata;
+package org.openmrs.module.esaudemetadata;
 
 
 import org.apache.commons.logging.Log; 
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.metadatasharing.ImportConfig;
+import org.openmrs.module.metadatasharing.ImportMode;
+import org.openmrs.module.metadatasharing.ImportedPackage;
+import org.openmrs.module.metadatasharing.MetadataSharing;
+import org.openmrs.module.metadatasharing.api.MetadataSharingService;
+import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
@@ -24,6 +35,9 @@ import org.openmrs.module.ModuleActivator;
 public class ESaudeMetadataActivator implements ModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
+
+	private static final String METADATA_FILENAME_FORMS = "MCH_Forms-1.zip";
+	private static final String METADATA_FILENAME_REPORTS = "MCH_Reports-1.zip";
 		
 	/**
 	 * @see ModuleActivator#willRefreshContext()
@@ -50,6 +64,13 @@ public class ESaudeMetadataActivator implements ModuleActivator {
 	 * @see ModuleActivator#started()
 	 */
 	public void started() {
+		try {
+
+			setupInitialData();
+
+		} catch (Exception ex) {
+			throw new RuntimeException("Failed to setup initial data", ex);
+		}
 		log.info("eSaude Metadata Module started");
 	}
 	
@@ -65,6 +86,56 @@ public class ESaudeMetadataActivator implements ModuleActivator {
 	 */
 	public void stopped() {
 		log.info("eSaude Metadata Module stopped");
+	}
+
+	/**
+	 * Checks whether the given version of the MDS package has been installed yet, and if not, install it
+	 *
+	 * @param groupUuid
+	 * @param filename
+	 * @return whether any changes were made to the db
+	 * @throws IOException
+	 */
+	private boolean installMetadataPackageIfNecessary(String groupUuid, String filename) throws IOException {
+		//NameWithNoSpaces-v1.zip
+		try {
+			Matcher matcher = Pattern.compile("\\w+-(\\d+).zip").matcher(filename);
+			if (!matcher.matches())
+				throw new RuntimeException("Filename must match PackageNameWithNoSpaces-1.zip");
+			Integer version = Integer.valueOf(matcher.group(1));
+
+			ImportedPackage installed = Context.getService(MetadataSharingService.class).getImportedPackageByGroup(groupUuid);
+			if (installed != null && installed.getVersion() >= version) {
+				log.info("Metadata package " + filename + " is already installed with version " + installed.getVersion());
+				return false;
+			}
+
+			if (getClass().getClassLoader().getResource(filename) == null) {
+				throw new RuntimeException("Cannot find " + filename + " for group " + groupUuid + ". Make sure it's in api/src/main/resources");
+			}
+
+			PackageImporter metadataImporter = MetadataSharing.getInstance().newPackageImporter();
+			metadataImporter.setImportConfig(ImportConfig.valueOf(ImportMode.PARENT_AND_CHILD));
+			metadataImporter.loadSerializedPackageStream(getClass().getClassLoader().getResourceAsStream(filename));
+			metadataImporter.importPackage();
+			return true;
+		} catch (Exception ex) {
+			log.error("Failed to install metadata package " + filename, ex);
+			return false;
+		}
+	}
+
+	/**
+	 * Public for testing
+	 *
+	 * @return whether any changes were made to the db
+	 * @throws Exception
+	 */
+	public boolean setupInitialData() throws Exception {
+		boolean anyChanges = false;
+		anyChanges |= installMetadataPackageIfNecessary("5b34396e-eb82-11e5-89eb-10c37b209c79", METADATA_FILENAME_FORMS);
+		anyChanges |= installMetadataPackageIfNecessary("65c7b888-eb82-11e5-a0d6-10c37b209c79", METADATA_FILENAME_REPORTS);
+		return anyChanges;
 	}
 		
 }
