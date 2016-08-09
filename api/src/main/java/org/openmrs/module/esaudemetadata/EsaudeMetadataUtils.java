2e17e417b72172886c4151a1c19d7476d8dd4df4
ningosi <nicotwendelee@gmail.com>
2016-08-09 13:52:09 +0300

    Added consultations current story MDS package

--- api/src/main/java/org/openmrs/module/esaudemetadata/EsaudeMetadataUtils.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/EsaudeMetadataUtils.java
@@ -12,6 +12,7 @@ public class EsaudeMetadataUtils {
         public static final String METADATA_FILENAME_REPORTS = "MCH_Reports-1.zip";
         public static final String METADATA_FILENAME_FLAGS = "flags-1.zip";
         public static final String METADATA_CONSULTATION_COHORTS = "consultation_schedule_cohort-3.zip";
+        public static final String METADATA_CONSULTATION_CURRENT_STORY ="consultation_current_story-1.zip";
     }
 
     public static final class _PackageUuids{
@@ -19,5 +20,6 @@ public class EsaudeMetadataUtils {
         public static final String METADATA_REPORTS_GROUP_UUID = "9a6ce9a3-1758-474d-9b01-3f2023bfc795";
         public static final String METADATA_FLAGS_GROUP_UUID = "2c90f9a7-ca56-4ee9-9f3c-03cd670a8cb0";
         public static final String METADATA_CONSULTATION_COHORTS_GROUP_UUID = "c8e7a6ab-c061-4afd-a5ee-79247ba1c9c3";
+        public static final String METADATA_CONSULTATION_CURRENT_STORY_GROUP_UUID = "e9b78422-3092-4ecb-ab9b-2b9e60c767d7";
     }
 }
