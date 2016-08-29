4e53ff05474e40df1ed6a245534a666aacc9a893
ningosi <nicotwendelee@gmail.com>
2016-08-29 13:19:37 +0300

    Added form fields for vitals

--- api/src/main/java/org/openmrs/module/esaudemetadata/EsaudeMetadataUtils.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/EsaudeMetadataUtils.java
@@ -6,26 +6,28 @@ package org.openmrs.module.esaudemetadata;
  */
 public class EsaudeMetadataUtils {
 
-    public static final class _PackageNames{
+    public static final class _PackageNames {
 
         public static final String METADATA_CCR_CPN_MCH_FORMS = "CPN_CCR_MCH_Forms-3.zip";
         public static final String METADATA_FILENAME_REPORTS = "MCH_Reports-1.zip";
         public static final String METADATA_CONSULTATION_COHORTS = "consultation_schedule_cohort-3.zip";
-        public static final String METADATA_CONSULTATION_CURRENT_STORY ="consultation_current_story-1.zip";
-        public static final String METADATA_DIAGNOSIS_CONCEPTS ="Diagnosis_Concepts-1.zip";
+        public static final String METADATA_CONSULTATION_CURRENT_STORY = "consultation_current_story-1.zip";
+        public static final String METADATA_DIAGNOSIS_CONCEPTS = "Diagnosis_Concepts-1.zip";
         public static final String METADATA_DIAGNOSIS_ADULT_SCHEMA = "Diagnosis_Adult_Schema-1.zip";
         public static final String METADATA_PERSON_ATTRIBUTE_TYPE = "person_attributes-1.zip";
         public static final String METADATA_PATIENT_IDENTIFIER_TYPES = "patient_identifiers-1.zip";
+        public static final String METADATA_FORM_FIELDS = "form_fields-1.zip";
     }
 
-    public static final class _PackageUuids{
+    public static final class _PackageUuids {
         public static final String METADATA_CCR_CPN_MCH_FORMS_GROUP_UUID = "7fc45f1e-148b-44b0-aea9-5ab459aee534";
         public static final String METADATA_REPORTS_GROUP_UUID = "9a6ce9a3-1758-474d-9b01-3f2023bfc795";
         public static final String METADATA_CONSULTATION_COHORTS_GROUP_UUID = "c8e7a6ab-c061-4afd-a5ee-79247ba1c9c3";
         public static final String METADATA_CONSULTATION_CURRENT_STORY_GROUP_UUID = "e9b78422-3092-4ecb-ab9b-2b9e60c767d7";
-        public static final String METADATA_DIAGNOSIS_CONCEPTS_UUID ="75aaf4eb-8e4d-4e0a-a1a6-c2496eca2e58";
+        public static final String METADATA_DIAGNOSIS_CONCEPTS_UUID = "75aaf4eb-8e4d-4e0a-a1a6-c2496eca2e58";
         public static final String METADATA_DIAGNOSIS_ADULT_SCHEMA_GROUP_UUID = "fc94ac84-7ca2-4acd-8bfd-17f70b9d17aa";
         public static final String METADATA_PERSON_ATTRIBUTE_TYPE_GROUP_UUID = "a38b6cd7-2177-4549-8660-30def2bf4a56";
         public static final String METADATA_PATIENT_IDENTIFIER_TYPES_GROUP_UUID = "e2ad2438-6a7c-45ec-a13a-32550c8560e2";
+        public static final String METADATA_FORM_FIELDS_GROUP_UUID = "b51c174d-6a78-4ab6-839d-44c75b0bfb30";
     }
 }
