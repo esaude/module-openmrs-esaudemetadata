ab69d4ae2c47de8e37cf66103b45d3ac32da9ba4
ningosi <nicotwendelee@gmail.com>
2016-10-10 14:43:49 +0300

    Filtered out voided obs in the alerts

--- api/src/main/resources/sql/alerts/viral_load_dropped.sql
+++ api/src/main/resources/sql/alerts/viral_load_dropped.sql
@@ -6,7 +6,7 @@ SELECT user_id INTO @user_id FROM users where uuid='f9a19c60-57eb-11e6-a798-507b
 INSERT INTO patientflags_flag(`name`,`criteria`,`message`,`enabled`,`evaluator`,`description`,`creator`,`date_created`,`changed_by`,`date_changed`,`retired`,`retired_by`,`date_retired`,`retire_reason`,`uuid`,`priority_id`)
     VALUES('VIRAL LOAD DROPPED',
     'SELECT p.patient_id FROM patient p WHERE
-    (SELECT ob.value_numeric FROM obs ob WHERE concept_id=730 AND ob.obs_datetime=(SELECT MAX(ob1.obs_datetime) FROM obs ob1 WHERE ob1.obs_datetime < (SELECT MAX(ob2.obs_datetime) FROM obs ob2)) AND ob.value_numeric > 0.5 * (SELECT ob3.value_numeric FROM obs ob3 WHERE ob3.value_numeric IS NOT NULL AND ob3.obs_datetime = (SELECT MAX(ob4.obs_datetime) FROM obs ob4 WHERE ob4.concept_id=730)));',
+    (SELECT ob.value_numeric FROM obs ob WHERE concept_id=730 AND ob.voided = 0 AND ob.obs_datetime=(SELECT MAX(ob1.obs_datetime) FROM obs ob1 WHERE ob1.voided = 0 AND ob1.obs_datetime < (SELECT MAX(ob2.obs_datetime) FROM obs ob2)) AND ob.value_numeric > 0.5 * (SELECT ob3.value_numeric FROM obs ob3 WHERE ob3.value_numeric IS NOT NULL AND ob3.voided = 0 AND ob3.obs_datetime = (SELECT MAX(ob4.obs_datetime) FROM obs ob4 WHERE ob4.concept_id=730 AND ob4.voided = 0)));',
     'Viral Load Dropped! the patient is doing the Prophylaxis',
 	  1,
 	  'org.openmrs.module.patientflags.evaluator.SQLFlagEvaluator',
--- api/src/main/resources/sql/alerts/viralogical_failure.sql
+++ api/src/main/resources/sql/alerts/viralogical_failure.sql
@@ -6,7 +6,7 @@ SELECT user_id INTO @user_id FROM users where uuid='f9a19c60-57eb-11e6-a798-507b
 INSERT INTO patientflags_flag(`name`,`criteria`,`message`,`enabled`,`evaluator`,`description`,`creator`,`date_created`,`changed_by`,`date_changed`,`retired`,`retired_by`,`date_retired`,`retire_reason`,`uuid`,`priority_id`)
     VALUES('VIRALOGICAL FAILURE',
     'SELECT p.patient_id FROM patient p WHERE
-    (SELECT ob.concept_id FROM obs ob WHERE ob.person_id = p.patient_id AND ob.concept_id IN (856,1518) AND ob.value_numeric > 1000 AND ob.obs_datetime = (SELECT max(obs.obs_datetime) FROM obs obs WHERE obs.person_id=p.patient_id));',
+    (SELECT ob.concept_id FROM obs ob WHERE ob.person_id = p.patient_id AND ob.concept_id IN (856,1518) AND ob.value_numeric > 1000 AND ob.obs_datetime = (SELECT max(obs.obs_datetime) FROM obs obs WHERE obs.person_id=p.patient_id) AND ob.voided = 0);',
     'VL greater than 1000 copies / mL.  Patient is having a therapeutic failure',
 	  1,
 	  'org.openmrs.module.patientflags.evaluator.SQLFlagEvaluator',
