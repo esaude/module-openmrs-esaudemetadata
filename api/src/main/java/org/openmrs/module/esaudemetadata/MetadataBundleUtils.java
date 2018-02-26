3cb975bbf8ad3ddd869b5f24c14bed6873244a8e
ningosi <nicotwendelee@gmail.com>
2018-02-26 15:08:26 +0300

    Disable loading of example metadata using metadata deploy, waiting for real ones

--- api/src/main/java/org/openmrs/module/esaudemetadata/MetadataBundleUtils.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/MetadataBundleUtils.java
@@ -3,20 +3,20 @@ package org.openmrs.module.esaudemetadata;
 public class MetadataBundleUtils {
 
     public static class _Privilege {
-        public static final String VIEW_POC = "View poc details jsut test";
-        public static final String ADMIN_POC = "Administer poc just testing";
+        //public static final String VIEW_POC = "View poc details jsut test";
+        //public static final String ADMIN_POC = "Administer poc just testing";
     }
 
     public static class _Role {
-        public static final String POC = "Application: poc just testing";
+        //public static final String POC = "Application: poc just testing";
 
     }
     public static final class _EncounterType {
-        public static final String TEST_ENCOUNTER_TYPE = "1cc69298-d9bb-11e7-ae8d-7f6c9aa2b70b";
+       // public static final String TEST_ENCOUNTER_TYPE = "1cc69298-d9bb-11e7-ae8d-7f6c9aa2b70b";
 
     }
     public static final class _Form {
-        public static final String TEST_FORM = "43b74dfc-d9bb-11e7-bb6e-8b5c28081dfe";
+        //public static final String TEST_FORM = "43b74dfc-d9bb-11e7-bb6e-8b5c28081dfe";
     }
     public static final class _PatientIdentifierType {
 
--- api/src/main/java/org/openmrs/module/esaudemetadata/metadatadeploy/CommonMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/metadatadeploy/CommonMetadataBundle.java
@@ -17,20 +17,20 @@ public class CommonMetadataBundle extends AbstractMetadataBundle {
         // install the patient identifier types
         // install person attribute types
         //Install Encounter Type
-        install(encounterType("Test deploy encounter type", "This is a testing encounter type", MetadataBundleUtils._EncounterType.TEST_ENCOUNTER_TYPE));
+        //install(encounterType("Test deploy encounter type", "This is a testing encounter type", MetadataBundleUtils._EncounterType.TEST_ENCOUNTER_TYPE));
         //Install forms
-        install(form("Test deploy form", "The form to collect different data", MetadataBundleUtils._EncounterType.TEST_ENCOUNTER_TYPE, "1.0", MetadataBundleUtils._Form.TEST_FORM));
+        //install(form("Test deploy form", "The form to collect different data", MetadataBundleUtils._EncounterType.TEST_ENCOUNTER_TYPE, "1.0", MetadataBundleUtils._Form.TEST_FORM));
         //installing programs
         //install Locations
         // Install Encounter Role
         //global properties
 
         //installing privileges
-        install(privilege(MetadataBundleUtils._Privilege.ADMIN_POC, "Poc administration to be done just test"));
-        install(privilege(MetadataBundleUtils._Privilege.VIEW_POC, "Poc viewing to be done just test"));
+        //install(privilege(MetadataBundleUtils._Privilege.ADMIN_POC, "Poc administration to be done just test"));
+        //install(privilege(MetadataBundleUtils._Privilege.VIEW_POC, "Poc viewing to be done just test"));
         //Tying roles to privileges
-        install(role(MetadataBundleUtils._Role.POC, "Looks up patients just test", idSet(), idSet(
-                MetadataBundleUtils._Privilege.ADMIN_POC,
-                MetadataBundleUtils._Privilege.VIEW_POC)));
+        //install(role(MetadataBundleUtils._Role.POC, "Looks up patients just test", idSet(), idSet(
+         //       MetadataBundleUtils._Privilege.ADMIN_POC,
+           //     MetadataBundleUtils._Privilege.VIEW_POC)));
     }
 }
