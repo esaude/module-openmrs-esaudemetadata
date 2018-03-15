26ab0ffbba038456a185aaa8e413568af2015aec
Edrisse Mussá <31036743+edrisse@users.noreply.github.com>
2018-03-15 11:58:00 +0200

    adding privileges for test order on POC (#7)

--- api/src/main/java/org/openmrs/module/esaudemetadata/MetadataBundleUtils.java
+++ api/src/main/java/org/openmrs/module/esaudemetadata/MetadataBundleUtils.java
@@ -2,95 +2,105 @@ package org.openmrs.module.esaudemetadata;
 
 public class MetadataBundleUtils {
 
-    public static class _Privilege {
-        //public static final String VIEW_POC = "View poc details jsut test";
-        //public static final String ADMIN_POC = "Administer poc just testing";
-    }
+	public static class _Privilege {
+		public static final String WRITE_TEST_ORDER = "Write Test Order";
+		public static final String READ_TEST_ORDER = "Read Test Order";
+		public static final String UPDATE_TEST_ORDER = "Update Test Order";
+	}
 
-    public static class _Role {
-        //public static final String POC = "Application: poc just testing";
+	public static class _Role {
+		public static final String POC_CLINICIAN = "POC: Clinician";
 
-    }
-    public static final class _EncounterType {
-       public static final String  CCU_RASTREIO = "e2791f26-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  ADMISSION = "9cf2aabe-10e8-11e5-9009-0242ac110012";
-       public static final String  APSS_PREVENCAO_POSITIVA = "f0bc6401-4e62-43e5-97e2-6ab027bb0405";
-       public static final String  APSS_PREVENCAO_POSITIVA_SEGUIMENTO = "f4aa93fe-8737-4f70-9532-542b768459d7";
-       public static final String  CCR_PCR = "e2791800-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  CCR_PROCESSO = "e278fbb8-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  CCR_SEGUIMENTO = "e278fe10-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  Consultation = "536111da-10e7-11e5-9009-0242ac110012";
-       public static final String  DISCHARGE = "9cf4a2cd-10e8-11e5-9009-0242ac110012";
-       public static final String  INVESTIGATION = "9d0c3be0-10e8-11e5-9009-0242ac110012";
-       public static final String  S_TARV_ADULTO_INICIAL_A = "e278f820-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  LAB_RESULT = "9d12403c-10e8-11e5-9009-0242ac110012";
-       public static final String  MISAU_ATS = "e27910d0-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  MISAU_ITS = "e278f460-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  MISAU_LABORATORIO = "e2790f68-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  POC_CURRENT_STORY = "782da6c5-3931-4ab5-8e10-2c647ee1cf9d";
-       public static final String  POC_DISPENSATION = "18fd49b7-6c2b-4604-88db-b3eb5b3a6d5f";
-       public static final String  PTV_MATERNIDADE = "e2791936-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  PTV_PRE_NATAL_INICIAL = "e278ff3c-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  PTV_PRE_NATAL_SEGUIMENTO = "e2790072-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_ACONSELHAMENTO = "e2791472-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_ACONSELHAMENTO_SEGUIMENTO = "e2791a62-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_ADULTO_INICIAL_B = "e278f1c2-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_ADULTO_SEGUIMENTO = "e278f956-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_AVALIACAO_DE_ADESAO = "b2ea993c-cb38-4d48-aade-00e62db9a57e";
-       public static final String  S_TARV_AVALIACAO_E_PREPARACAO_DO_CANDIDATO_TARV = "f69df47f-44a2-4cca-9819-a536bd47d927";
-       public static final String  S_TARV_BUSCA_ACTIVA = "e27916d4-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_FARMACIA = "e279133c-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_LIVRO_PRE_TARV = "bc30cbe1-85b4-459f-acca-cffc4041a3b8";
-       public static final String  S_TARV_LIVRO_TARV = "15915fa8-3d94-430b-9adb-14ec806a8ff2";
-       public static final String  S_TARV_PEDIATRIA_INICIAL_A = "e278fa8c-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_PEDIATRIA_INICIAL_B = "e278f5aa-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_PEDIATRIA_SEGUIMENTO = "e278fce4-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_SOLICITACAO_ARV = "e2791206-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  S_TARV_TERMO_DE_CONSENTIMENTO_DE_VISITA = "f719006d-dd1f-44cc-b4a0-d25257a8f558";
-       public static final String  TRANSFER = "9cf6f812-10e8-11e5-9009-0242ac110012";
-       public static final String  TUBERCULOSE_LIVRO = "e2791b98-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  TUBERCULOSE_PROCESSO = "e2791cc4-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  TUBERCULOSE_RASTREIO = "e27915a8-1d5f-11e0-b929-000c29ad1d07";
-       public static final String  TUBERCULOSE_SEGUIMENTO = "e2791dfa-1d5f-11e0-b929-000c29ad1d07";
-  }
-    public static final class _Form {
-        public static final String ADULTO_PROCESSO_PARTE_A_ANAMNESE = "e28b6096-1d5f-11e0-b929-000c29ad1d07";
-    }
-    public static final class _PatientIdentifierType {
+	}
 
-        public static final String NID_SERVICO_TARV = "e2b966d0-1d5f-11e0-b929-000c29ad1d07";
-        public static final String BILHETE_DE_IDENTIDADE_BI = "e2b9682e-1d5f-11e0-b929-000c29ad1d07";
-        public static final String CODIGO_ATS_UATS = "e2b9698c-1d5f-11e0-b929-000c29ad1d07";
-        public static final String CODIGO_ITS = "e2b96c16-1d5f-11e0-b929-000c29ad1d07";
-        public static final String CODIGO_PTV_MATERNIDADE = "e2b96d56-1d5f-11e0-b929-000c29ad1d07";
-        public static final String CODIGO_PTV_PRE_NATAL = "e2b96ad6-1d5f-11e0-b929-000c29ad1d07";
-        public static final String NID_CCR = "e2b97b70-1d5f-11e0-b929-000c29ad1d07";
-        public static final String NIT_SECTOR_DE_TB = "e2b97e40-1d5f-11e0-b929-000c29ad1d07";
-        public static final String NUMERO_CANCRO_CERVICAL = "e2b97f8a-1d5f-11e0-b929-000c29ad1d07";
-        public static final String PCR_NUMERO_DE_REGISTO = "e2b97cec-1d5f-11e0-b929-000c29ad1d07";
+	public static final class _EncounterType {
+		public static final String CCU_RASTREIO = "e2791f26-1d5f-11e0-b929-000c29ad1d07";
+		public static final String ADMISSION = "9cf2aabe-10e8-11e5-9009-0242ac110012";
+		public static final String APSS_PREVENCAO_POSITIVA = "f0bc6401-4e62-43e5-97e2-6ab027bb0405";
+		public static final String APSS_PREVENCAO_POSITIVA_SEGUIMENTO = "f4aa93fe-8737-4f70-9532-542b768459d7";
+		public static final String CCR_PCR = "e2791800-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CCR_PROCESSO = "e278fbb8-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CCR_SEGUIMENTO = "e278fe10-1d5f-11e0-b929-000c29ad1d07";
+		public static final String Consultation = "536111da-10e7-11e5-9009-0242ac110012";
+		public static final String DISCHARGE = "9cf4a2cd-10e8-11e5-9009-0242ac110012";
+		public static final String INVESTIGATION = "9d0c3be0-10e8-11e5-9009-0242ac110012";
+		public static final String S_TARV_ADULTO_INICIAL_A = "e278f820-1d5f-11e0-b929-000c29ad1d07";
+		public static final String LAB_RESULT = "9d12403c-10e8-11e5-9009-0242ac110012";
+		public static final String MISAU_ATS = "e27910d0-1d5f-11e0-b929-000c29ad1d07";
+		public static final String MISAU_ITS = "e278f460-1d5f-11e0-b929-000c29ad1d07";
+		public static final String MISAU_LABORATORIO = "e2790f68-1d5f-11e0-b929-000c29ad1d07";
+		public static final String POC_CURRENT_STORY = "782da6c5-3931-4ab5-8e10-2c647ee1cf9d";
+		public static final String POC_DISPENSATION = "18fd49b7-6c2b-4604-88db-b3eb5b3a6d5f";
+		public static final String PTV_MATERNIDADE = "e2791936-1d5f-11e0-b929-000c29ad1d07";
+		public static final String PTV_PRE_NATAL_INICIAL = "e278ff3c-1d5f-11e0-b929-000c29ad1d07";
+		public static final String PTV_PRE_NATAL_SEGUIMENTO = "e2790072-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_ACONSELHAMENTO = "e2791472-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_ACONSELHAMENTO_SEGUIMENTO = "e2791a62-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_ADULTO_INICIAL_B = "e278f1c2-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_ADULTO_SEGUIMENTO = "e278f956-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_AVALIACAO_DE_ADESAO = "b2ea993c-cb38-4d48-aade-00e62db9a57e";
+		public static final String S_TARV_AVALIACAO_E_PREPARACAO_DO_CANDIDATO_TARV = "f69df47f-44a2-4cca-9819-a536bd47d927";
+		public static final String S_TARV_BUSCA_ACTIVA = "e27916d4-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_FARMACIA = "e279133c-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_LIVRO_PRE_TARV = "bc30cbe1-85b4-459f-acca-cffc4041a3b8";
+		public static final String S_TARV_LIVRO_TARV = "15915fa8-3d94-430b-9adb-14ec806a8ff2";
+		public static final String S_TARV_PEDIATRIA_INICIAL_A = "e278fa8c-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_PEDIATRIA_INICIAL_B = "e278f5aa-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_PEDIATRIA_SEGUIMENTO = "e278fce4-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_SOLICITACAO_ARV = "e2791206-1d5f-11e0-b929-000c29ad1d07";
+		public static final String S_TARV_TERMO_DE_CONSENTIMENTO_DE_VISITA = "f719006d-dd1f-44cc-b4a0-d25257a8f558";
+		public static final String TRANSFER = "9cf6f812-10e8-11e5-9009-0242ac110012";
+		public static final String TUBERCULOSE_LIVRO = "e2791b98-1d5f-11e0-b929-000c29ad1d07";
+		public static final String TUBERCULOSE_PROCESSO = "e2791cc4-1d5f-11e0-b929-000c29ad1d07";
+		public static final String TUBERCULOSE_RASTREIO = "e27915a8-1d5f-11e0-b929-000c29ad1d07";
+		public static final String TUBERCULOSE_SEGUIMENTO = "e2791dfa-1d5f-11e0-b929-000c29ad1d07";
+	}
 
-    }
-    public static final class _Program {
+	public static final class _Form {
+		public static final String ADULTO_PROCESSO_PARTE_A_ANAMNESE = "e28b6096-1d5f-11e0-b929-000c29ad1d07";
+	}
 
-    }
-    public static final class _PersonAttributeType {
-        public static final String NICK_NAME = "d82b0cf4-26cc-11e8-bdc0-2b5ea141f82e";
+	public static final class _PatientIdentifierType {
 
-    }
-    public static final class _VisitAttributeType {
+		public static final String NID_SERVICO_TARV = "e2b966d0-1d5f-11e0-b929-000c29ad1d07";
+		public static final String BILHETE_DE_IDENTIDADE_BI = "e2b9682e-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CODIGO_ATS_UATS = "e2b9698c-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CODIGO_ITS = "e2b96c16-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CODIGO_PTV_MATERNIDADE = "e2b96d56-1d5f-11e0-b929-000c29ad1d07";
+		public static final String CODIGO_PTV_PRE_NATAL = "e2b96ad6-1d5f-11e0-b929-000c29ad1d07";
+		public static final String NID_CCR = "e2b97b70-1d5f-11e0-b929-000c29ad1d07";
+		public static final String NIT_SECTOR_DE_TB = "e2b97e40-1d5f-11e0-b929-000c29ad1d07";
+		public static final String NUMERO_CANCRO_CERVICAL = "e2b97f8a-1d5f-11e0-b929-000c29ad1d07";
+		public static final String PCR_NUMERO_DE_REGISTO = "e2b97cec-1d5f-11e0-b929-000c29ad1d07";
 
-    }
-    public static final class _VisitType {
+	}
 
-    }
-    public static final class _RelationshipType {
+	public static final class _Program {
 
-    }
-    public static final class _GlobalProperties {
+	}
 
-    }
+	public static final class _PersonAttributeType {
+		public static final String NICK_NAME = "d82b0cf4-26cc-11e8-bdc0-2b5ea141f82e";
 
-    public static final class _LocationAttributeType {
-        public static final String MASTER_FACILITY_CODE = "132895aa-1c88-11e8-b6fd-7395830b63f3";
-    }
+	}
+
+	public static final class _VisitAttributeType {
+
+	}
+
+	public static final class _VisitType {
+
+	}
+
+	public static final class _RelationshipType {
+
+	}
+
+	public static final class _GlobalProperties {
+
+	}
+
+	public static final class _LocationAttributeType {
+		public static final String MASTER_FACILITY_CODE = "132895aa-1c88-11e8-b6fd-7395830b63f3";
+	}
 }
